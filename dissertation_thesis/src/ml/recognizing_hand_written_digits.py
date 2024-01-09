5d8ab7b8196daef2912614f7bef395905c393c3d
trincema <emanuel.trinc@yahoo.com>
2024-01-09 17:41:51 +0200

    traffic analyzer

--- dissertation_thesis/src/ml/recognizing_hand_written_digits.py
+++ dissertation_thesis/src/ml/recognizing_hand_written_digits.py
@@ -9,19 +9,32 @@ from sklearn import datasets, metrics, svm
 from sklearn.model_selection import train_test_split
 
 digits = datasets.load_digits()
-print(digits.data)
-print("images: " + str(len(digits.images)) +
-    " of size (" + str(len(digits.images[0])) +
-    ", " + str(len(digits.images[0][0])) + ")")
-print("data set size: rows = " + str(len(digits.data)) + " columns = " + str(len(digits.data[0])))
-print("target: " + str(digits.target) + " size: " + str(len(digits.target)))
+# flatten the images
+n_samples = len(digits.images)
+n_features = len(digits.target)
+data = digits.images.reshape((n_samples, -1))
+# Create a classifier: a support vector classifier
+clf = svm.SVC(gamma=0.001)
+# Split data into 50% train and 50% test subsets
+x_train, x_test, y_train, y_test = train_test_split(
+    data, digits.target, test_size=0.5, shuffle=False
+)
+clf.fit(x_train, y_train)
+predicted = clf.predict(x_test)
 
-# PERFORM TRAINING
 _, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
-for ax, image, label in zip(axes, digits.images, digits.target):
+for ax, image, prediction in zip(axes, x_test, predicted):
     ax.set_axis_off()
+    image = image.reshape(8, 8)
     ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
-    ax.set_title("Training: %i" % label)
+    ax.set_title(f"Prediction: {prediction}")
 
-print(digits.images)
-print("n_samples = " + str(len(digits.images)))
+print(
+    f"Classification report for classifier {clf}:\n"
+    f"{metrics.classification_report(y_test, predicted)}\n"
+)
+
+disp = metrics.ConfusionMatrixDisplay.from_predictions(y_test, predicted)
+disp.figure_.suptitle("Confusion Matrix")
+print(f"Confusion matrix:\n{disp.confusion_matrix}")
+plt.show()
